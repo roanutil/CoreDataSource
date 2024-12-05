a377ab766b7be07fc81ea7f2a4cb8c1f1b9dc797
Andrew Roan <roanutil@users.noreply.github.com>
2024-12-04 22:01:00 -0600

    Relax RepositoryManagedModel.Unmanaged requirements to allow building with Swift 6
    bugfix/unable-to-build-v2-with-swift-6

--- Sources/CoreDataRepository/RepositoryManagedModel.swift
+++ Sources/CoreDataRepository/RepositoryManagedModel.swift
@@ -10,7 +10,7 @@ import CoreData
 
 /// A protocol for a CoreData NSManagedObject sub class that has a corresponding value type
 public protocol RepositoryManagedModel: NSManagedObject {
-    associatedtype Unmanaged: UnmanagedModel where Unmanaged.RepoManaged == Self
+    associatedtype Unmanaged: UnmanagedModel
     /// Returns a value type instance of `self`
     var asUnmanaged: Unmanaged { get }
     /// Create `self` from a corresponding instance of `UnmanagedModel`. Should not save the context.
